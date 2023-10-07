37540487c8bd593acf2dc02cb8ebab06af7ef3cb
Meysam Karimi <meysam.k1717@gmail.com>
2023-10-07 11:07:24 -0400

    refactor: Change user id migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->unsignedInteger('id')->autoIncrement();
             $table->unsignedInteger('telegram_id');
             $table->string('first_name')->nullable();
             $table->string('last_name')->nullable();
