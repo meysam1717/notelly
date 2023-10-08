525a3679ec029e82c2b5b7d033d1f006a211d451
Meysam Karimi <meysam.k1717@gmail.com>
2023-10-08 11:12:26 -0400

    refactor: Change StoreFolderUseCase to support with user id

--- app/UseCases/Folder/StoreFolderUseCase.php
+++ app/UseCases/Folder/StoreFolderUseCase.php
@@ -5,38 +5,58 @@ namespace App\UseCases\Folder;
 use App\Models\Folder;
 use App\Models\User;
 use App\Services\FolderService;
+use App\Services\UserService;
+use Exception;
 use Illuminate\Validation\ValidationException;
 
 class StoreFolderUseCase
 {
 
+    private int $userId;
     private User $user;
     private string $name;
 
     public function __construct(
         private readonly FolderService $folderService,
+        private readonly UserService $userService,
     )
     {
     }
 
-    public function execute(User $user, string $name): Folder
+    /**
+     * @throws Exception
+     */
+    public function execute(int $userId, string $name): Folder
     {
-        $this->user = $user;
+        $this->userId = $userId;
         $this->name = $name;
-        return $this->checkFolderDuplicate()
+        return $this->getUser()
+            ->checkFolderDuplicate()
             ->createFolder();
 
     }
 
+    /**
+     * @throws Exception
+     */
+    private function getUser(): self
+    {
+        $user = $this->userService->getUserById($this->userId);
+        if (!$user){
+            throw new Exception('User not found');
+        }
+        $this->user = $user;
+        return $this;
+    }
 
     /**
-     * @throws ValidationException
+     * @throws Exception
      */
     private function checkFolderDuplicate(): self
     {
         $folders = $this->folderService->getUserFoldersByName($this->user, $this->name);
         if (!empty($folders)){
-            throw new ValidationException("Folder with name '{$this->name}' exists");
+            throw new Exception("Folder with name '{$this->name}' exists");
         }
         return $this;
     }
