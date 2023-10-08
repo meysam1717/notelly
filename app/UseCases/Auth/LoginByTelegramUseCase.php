d0f425586c9f572176d070c3f0cf6f988c3cbb84
Meysam Karimi <meysam.k1717@gmail.com>
2023-10-08 10:01:49 -0400

    feat: Change error handling of login by telegram use case

--- app/UseCases/Auth/LoginByTelegramUseCase.php
+++ app/UseCases/Auth/LoginByTelegramUseCase.php
@@ -10,6 +10,7 @@ use App\Services\FolderService;
 use App\Services\UserService;
 use App\Traits\TelegramTrait;
 use Exception;
+use Illuminate\Validation\ValidationException;
 
 class LoginByTelegramUseCase
 {
@@ -22,52 +23,55 @@ class LoginByTelegramUseCase
     private bool $userNotExists;
 
     public function __construct(
-        private readonly UserService $userService,
-        private readonly AuthService $authService,
+        private readonly UserService   $userService,
+        private readonly AuthService   $authService,
         private readonly FolderService $folderService,
     )
     {
     }
 
+    /**
+     * @throws ValidationException
+     */
     public function execute(string $initData): bool
     {
         $this->initData = $initData;
-        try {
-            return $this->isTelegramDataValid()
-                ->extractTelegramData()
-                ->getUserByTelegramId()
-                ->createUserByTelegramIdIfNotExists()
-                ->createDefaultFolderIfUserDoesNotExists()
-                ->loginUser();
-        } catch (Exception $e) {
-            return false;
-        }
+        return $this->isTelegramDataValid()
+            ->extractTelegramData()
+            ->getUserByTelegramId()
+            ->createUserByTelegramIdIfNotExists()
+            ->createDefaultFolderIfUserDoesNotExists()
+            ->loginUser();
     }
 
     /**
-     * @throws Exception
+     * @throws ValidationException
      */
     private function isTelegramDataValid(): self
     {
-        $isValid = $this->validateInitData($this->initData);
-        if (!$isValid) {
-            throw new Exception('Telegram data is not valid');
+        try {
+            $isValid = $this->validateInitData($this->initData);
+            if (!$isValid) {
+                throw new Exception();
+            }
+        } catch (Exception $e) {
+            throw new ValidationException('Telegram data is not valid');
         }
         return $this;
     }
 
     private function extractTelegramData(): self
     {
-       $this->telegramInitDataDto = $this->extractInitData($this->initData);
-       return $this;
+        $this->telegramInitDataDto = $this->extractInitData($this->initData);
+        return $this;
     }
 
     private function getUserByTelegramId(): self
     {
         $user = $this->userService->getUserByTelegramId($this->telegramInitDataDto->getTelegramUserDto()->getId());
-        if(empty($user)){
+        if (empty($user)) {
             $this->userNotExists = true;
-        }else{
+        } else {
             $this->userNotExists = false;
             $this->user = $user;
         }
@@ -76,7 +80,7 @@ class LoginByTelegramUseCase
 
     private function createUserByTelegramIdIfNotExists(): self
     {
-        if ($this->userNotExists){
+        if ($this->userNotExists) {
             $this->user = $this->userService->createUserByTelegramData($this->telegramInitDataDto->getTelegramUserDto());
         }
         return $this;
@@ -84,8 +88,8 @@ class LoginByTelegramUseCase
 
     private function createDefaultFolderIfUserDoesNotExists(): self
     {
-        if ($this->userNotExists){
-            $this->folderService->createFolder($this->user->getId(), AppConstants::DEFAULT_FOLDER_NAME);
+        if ($this->userNotExists) {
+            $this->folderService->createFolder($this->user, AppConstants::DEFAULT_FOLDER_NAME);
         }
         return $this;
     }
