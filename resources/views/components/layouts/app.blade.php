c0f35c032339a77482a66be4d3242e001ca56a7c
meysam hosseini <meysamhosseini1995@gmail.com>
2023-10-09 11:08:19 +0330

    feat: make main layout livewire and config telegram script

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -4,15 +4,41 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <title>{{ $title ?? 'Page Title' }}</title>
+    <title>{{ $title ?? 'Anitec Team' }}</title>
     <script src="https://telegram.org/js/telegram-web-app.js"></script>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <script>
+        let telegram = window.Telegram.WebApp;
+        // telegram.isClosingConfirmationEnabled = true;
+        // telegram.enableClosingConfirmation = true;
+    </script>
+    @vite(['resources/css/app.css','resources/js/app.js'])
+    @livewireStyles
 </head>
 
 <body>
+
 {{ $slot }}
 
+
+<script>
+    telegram.BackButton.isVisible = true;
+    telegram.BackButton.show();
+    telegram.BackButton.onClick(function (event) {
+        if (window.location.pathname === '/'){
+            telegram.close();
+        }else{
+            history.back();
+        }
+    })
+
+    document.addEventListener('livewire:initialized', () => {
+        Livewire.on('notify', (event) => {
+            telegram.showAlert(event.message)
+        });
+    });
+</script>
+@stack('js')
+@livewireScriptConfig
 </body>
 
 </html>
