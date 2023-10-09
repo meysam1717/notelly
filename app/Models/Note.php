46f8b00e7ac1f24d51a4eaacae532d5fc3ef53e7
Meysam Karimi <meysam.k1717@gmail.com>
2023-10-09 03:32:09 -0400

    feat: Add title to note

--- app/Models/Note.php
+++ app/Models/Note.php
@@ -7,13 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 /**
  * @property integer $id
  * @property integer $folder_id
- * @property array $data
+ * @property string $title
+ * @property ?array $data
  */
 class Note extends Model
 {
 
     protected $fillable = [
         'folder_id',
+        'title',
         'data',
     ];
 
@@ -31,19 +33,38 @@ class Note extends Model
         return $this->folder_id;
     }
 
-    public function setFolderId(int $folder_id): void
+    public function setFolderId(int $folder_id): self
     {
         $this->folder_id = $folder_id;
+        return $this;
     }
 
-    public function getData(): array
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+        return $this;
+    }
+
+    /**
+     * @return ?array<string, mixed>
+     */
+    public function getData(): ?array
     {
         return $this->data;
     }
 
-    public function setData(array $data): void
+    /**
+     * @param ?array<string, mixed> $data
+     */
+    public function setData(?array $data): self
     {
         $this->data = $data;
+        return $this;
     }
 
 }
--- database/migrations/2023_10_07_162348_create_notes_table.php
+++ database/migrations/2023_10_07_162348_create_notes_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('notes', function (Blueprint $table) {
             $table->unsignedInteger('id')->autoIncrement();
             $table->unsignedInteger('folder_id');
+            $table->string('title', 31);
             $table->json('data');
             $table->timestamps();
         });
