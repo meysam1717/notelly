2fcfe3d3ed6ed2f225d33b49803d857bbde49e8f
meysam hosseini <meysamhosseini1995@gmail.com>
2023-10-09 11:05:44 +0330

    feat: add livewire assets with laravel vite

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,12 @@
-import './bootstrap';
+import {Livewire, Alpine} from '../../vendor/livewire/livewire/dist/livewire.esm';
+import Clipboard from '@ryangjchandler/alpine-clipboard'
+Alpine.plugin(Clipboard)
+Livewire.start()
+
+Livewire.hook('request', ({uri, options, payload, respond, succeed, fail}) => {
+    options.headers = {
+        "Content-type": "application/json",
+        "init-data": telegram.initData,
+        "X-Livewire": ""
+    };
+})
