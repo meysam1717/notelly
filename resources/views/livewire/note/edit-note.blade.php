bf0af2d935d90c0769623e302dfc658f8474f512
meysam hosseini <meysamhosseini1995@gmail.com>
2023-10-09 16:33:02 +0330

    feat: make edit note livewire component and develop for edit note

---
+++ resources/views/livewire/note/edit-note.blade.php
@@ -0,0 +1,27 @@
+<div class="w-full max-w-3xl p-4 ">
+    <h1 class="mb-2 font-semibold text-center text-typograph-header">Edit Note</h1>
+    <x-forms.input model="title" lable="Title" type="text"/>
+    <x-forms.editorjs lable="Note"/>
+
+</div>
+
+
+@push('js')
+    <script type="text/javascript">
+        document.addEventListener('livewire:initialized', () => {
+            Livewire.dispatch('get-note-info',{'folderId':{{ $folderId}} ,'noteId':{{ $noteId }} });
+            telegram.MainButton.text = "Save";
+            telegram.MainButton.show();
+            telegram.MainButton.onClick(function () {
+                updateNote();
+            })
+        });
+
+
+        function updateNote() {
+            editor.save().then((outputData) => {
+                Livewire.dispatch('update-note',{'note':outputData,'folderId':{{ $folderId}} ,'noteId':{{ $noteId }} });
+            });
+        }
+    </script>
+@endpush
