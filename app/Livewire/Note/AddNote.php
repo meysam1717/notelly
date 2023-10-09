eb0761b0e726267ffb22e13f0a0be3c4de85c9ba
meysam hosseini <meysamhosseini1995@gmail.com>
2023-10-09 16:32:22 +0330

    feat: make add note livewire component and develop save new note

---
+++ app/Livewire/Note/AddNote.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Livewire\Note;
+
+use App\UseCases\Note\StoreNoteUseCase;
+use Exception;
+use Illuminate\Contracts\View\View;
+use Illuminate\Support\Facades\Auth;
+use Livewire\Attributes\On;
+use Livewire\Attributes\Rule;
+use Livewire\Component;
+
+class AddNote extends Component
+{
+    private StoreNoteUseCase $storeNoteUseCase;
+
+    #[Rule('required|min:2|max:30')]
+    public string $title = '';
+
+    public mixed $note;
+
+
+    public function boot(StoreNoteUseCase $storeNoteUseCase): void
+    {
+        $this->storeNoteUseCase = $storeNoteUseCase;
+    }
+
+
+    /**
+     * @throws Exception
+     */
+    #[On('save-new-note')]
+    public function save($note,$folderId)
+    {
+        $this->validate();
+        $this->storeNoteUseCase->execute(Auth::id(),$folderId,$this->title,$note);
+        $this->reset(['title']);
+        $this->dispatch('notify', message: "Note Saved");
+        return redirect()->route('note-list',['id'=>$folderId]);
+    }
+
+
+    /**
+     * @return View
+     */
+    public function render(): View
+    {
+        return view('livewire.note.add-note');
+    }
+}
